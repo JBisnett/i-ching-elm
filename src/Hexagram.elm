cf14301b1257d70192cabe9d57d28c54d4eadf82
Jacob Bisnett <jacob.bisnett@gmail.com>
2017-04-24 11:39:34 -0400

    start formatting the hexagrams

--- src/Hexagram.elm
+++ src/Hexagram.elm
@@ -7,26 +7,73 @@ import Material.Options as Options exposing (css)
 type alias Hexagram = 
   { number: Int
   , name: String
+  , directName: String
   , character: String
-  , meaning: String
+  , image: String
+  , judgement: String
   , lines: List String
   , repr: List Bool
-  , image: String
-  , sequence: String 
+  , picture: String
   }
 
 hexagramList : List Hexagram
-hexagramList = [ { number = 1, name = "Test", character = "", meaning = "", lines = [""], repr = [True, True, True, True, True, True], image = "", sequence = "" } ]
+hexagramList = 
+  [ { number = 1
+    , name = "The Creative"
+    , directName = "Ch'ien"
+    , character = "䷀"
+    , judgement = "The Creative works sublime success, Furthering through perseverance."
+    , image = "The movement of heaven is full of power. Thus the superior man makes himself strong and untiring."
+    , lines = 
+      [ "Nine at the beginning means: Hidden dragon. Do not act."
+      , "Nine in the second place means: Dragon appearing in the field. It furthers one to see the great man."
+      , "Nine in the third place means: All day long the superior man is creatively active. At nightfall his mind is still beset with cares. Danger. No blame."
+      , "Nine in the fourth place means: Wavering flight over the depths. No blame."
+      , "Nine in the fifth place means: Flying dragon in the heavens. It furthers one to see the great man."
+      , "Nine at the top means: Arrogant dragon will have cause to repent."
+      ]
+    , repr = [True, True, True, True, True, True]
+    , picture = ""
+    }
+  , { number = 2
+    , name = "The Receptive"
+    , directName = "K'un"
+    , character = "䷁"
+    , judgement = "The Creative works sublime success, Furthering through perseverance."
+    , image = "The movement of heaven is full of power. Thus the superior man makes himself strong and untiring."
+    , lines = 
+      [ "Nine at the beginning means: Hidden dragon. Do not act."
+      , "Nine in the second place means: Dragon appearing in the field. It furthers one to see the great man."
+      , "Nine in the third place means: All day long the superior man is creatively active. At nightfall his mind is still beset with cares. Danger. No blame."
+      , "Nine in the fourth place means: Wavering flight over the depths. No blame."
+      , "Nine in the fifth place means: Flying dragon in the heavens. It furthers one to see the great man."
+      , "Nine at the top means: Arrogant dragon will have cause to repent."
+      ]
+    , repr = [False, False, False, False, False, False]
+    , picture = ""
+    }
+  ]
+
+toHtml: Hexagram -> Html msg
+toHtml hex = 
+  div [] 
+    [ h1 [] [text hex.character]
+    , h2 [] [text hex.directName]
+    , h3 [] [text hex.name]
+    , h4 [] [text "Judgement"]
+    , p [] [text hex.judgement]
+    , h4 [] [text "Image"]
+    , p [] [text hex.image]
+    , h4 [] [text "Lines"]
+    ]
 
 renderReading: Hexagram -> Hexagram -> Html msg
 renderReading sgram mgram = 
-  grid []
-    [ cell [size All 10, Color.background <| Color.primary, Options.center]
-      [ h4 [] [text "Initial Hexagram"]
-      ]
-    , cell [size All 4, Color.background <| Color.accent, Options.center]
-      [ 
-        div [] [ h4 [] [text "Resultant Hexagram"]
-               , p [] [text "Offset by 2"] ]
-      ]
-    ]
+  grid [Options.center]
+  [ cell [size All 5, Color.background <| Color.primary, Color.text Color.primaryContrast, Options.center]
+  [ toHtml sgram
+  ]
+  , cell [size All 5, Color.background <| Color.primaryDark, Color.text Color.primaryContrast, Options.center]
+  [ toHtml mgram 
+  ]
+  ]
